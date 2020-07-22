f448c279b2740d6ad89499459e813ed63f54fa78
Manan Malhotra <mananmalhotra80@gmail.com>
2020-07-22 12:21:41 +0530

    Added async/await

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-// const { db } = require('./models/user');
-
 const express = require('express'),
 	mongoose = require('mongoose');
 require('./db/mongoose');
@@ -12,32 +10,77 @@ port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Routes
-app.post('/users', (req, res) => {
+
+// Post requests
+app.post('/users', async (req, res) => {
 	const user = new User(req.body);
 
-	user
-		.save()
-		.then(() => {
-			res.status(201).send(user);
-		})
-		.catch((err) => {
-			res.status(400).send(err);
-		});
+	try {
+		await user.save();
+		res.status(201).send(user);
+	} catch (e) {
+		res.status(400).send(err);
+	}
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
 	const task = new Task(req.body);
 
-	task
-		.save()
-		.then(() => {
-			res.status(201).send(task);
-		})
-		.catch((err) => {
-			res.status(400).send(err);
-		});
+	try {
+		await task.save();
+		res.status(201).send(task);
+	} catch (e) {
+		res.status(400).send(err);
+	}
+});
+// Get Request
+app.get('/users', async (req, res) => {
+	try {
+		const users = await User.find({});
+		res.send(users);
+	} catch (e) {
+		res.status(500).send(e);
+	}
 });
 
+app.get('/users/:id', async (req, res) => {
+	const _id = req.params.id;
+
+	try {
+		const user = await User.findById({ _id });
+		if (!user) {
+			return res.status(404).send();
+		}
+		res.send(user);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
+app.get('/tasks', async (req, res) => {
+	try {
+		const task = await Task.find({});
+		res.send(task);
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
+app.get('/tasks/:id', async (req, res) => {
+	const _id = req.params.id;
+
+	try {
+		const task = await Task.findById({ _id });
+		if (!task) {
+			return res.status(404).send();
+		}
+		res.send(task);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
+// Listen
 app.listen(port, () => {
 	console.log('Server started at port ' + port);
 });
